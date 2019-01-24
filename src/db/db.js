3d69e6f5e6ade84a16d35afdb73554f89fef785e
hujiangyi <hujiangyi@dvt.dvt.com>
2019-01-24 09:46:39 +0800

    修改mongodb参数

--- src/db/db.js
+++ src/db/db.js
@@ -1,16 +1,16 @@
 const config = require('config');
 const mongoose = require("mongoose");
 
-const mongodbip = config.get('licensesdb.ip');
-const mongodbport = config.get('licensesdb.port');
-const mongodbname = config.get('licensesdb.db');
-const mongodbusername = config.get('licensesdb.username');
-const mongodbpassword = config.get('licensesdb.password');
+const mongodbip = config.get('raspberrydb.ip');
+const mongodbport = config.get('raspberrydb.port');
+const mongodbname = config.get('raspberrydb.db');
+const mongodbusername = config.get('raspberrydb.username');
+const mongodbpassword = config.get('raspberrydb.password');
 
 const url = "mongodb://" +  mongodbusername + ":" + mongodbpassword + "@" + mongodbip + ":" + mongodbport + "/" + mongodbname;
 const db = mongoose.createConnection(url);
-db.on('error', console.error.bind(console, '... connect licensesdb error ...'));
+db.on('error', console.error.bind(console, '... connect raspberrydb error ...'));
 db.once('open', function callback() {
-    console.info("... db open licensesdb ...");
+    console.info("... db open raspberrydb ...");
 });
 module.exports = db;
